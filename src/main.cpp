8a02502d7e70b8c97ffb4aefca9c843c95d80d5c
Stuart Alldritt <s.k.alldritt@gmail.com>
2021-11-06 16:58:03 -0700

    Initial GLFW vulkan

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,13 @@
 #include <string>
 
 #include <fmt/core.h>
+
 #include <CLI/App.hpp>
 #include <CLI/Formatter.hpp>
 #include <CLI/Config.hpp>
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 using namespace std;
 
@@ -20,6 +24,25 @@ int main(int argc, char** argv)
     app.add_option("-f,--file", filename, "A help string");
 
     CLI11_PARSE(app, argc, argv);
+
+    GLFWwindow* window;
+
+    // Initialize GLFW
+    if (!glfwInit()) {
+        fmt::print("Failed to init GLFW\n");
+        return -1;
+    }
+
+    fmt::print("GLFW initialized\n");
+
+    if (glfwVulkanSupported())
+    {
+        // Vulkan is available, at least for compute
+        fmt::print("GLFW detected vulkan support\n");
+    } else {
+        fmt::print("GLFW did not detect vulkan support\n");
+    }
+
 
     return 0;
 }
