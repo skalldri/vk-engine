e61f863b3a993978fc02805861ccff8f19b1411a
Stuart Alldritt <s.k.alldritt@gmail.com>
2021-11-06 18:34:55 -0700

    Barebones vulkan setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <exception>
 
 #include <fmt/core.h>
 
@@ -11,6 +12,246 @@
 #include <GLFW/glfw3.h>
 
 using namespace std;
+
+GLFWwindow* window;
+VkInstance instance;
+VkDebugUtilsMessengerEXT debugMessenger;
+
+constexpr size_t window_width = 800;
+constexpr size_t window_height = 600;
+
+const std::vector<const char*> validationLayers = {
+    "VK_LAYER_KHRONOS_validation"
+};
+
+#ifdef DEBUG_BUILD
+constexpr bool enableValidationLayers = true;
+#else
+constexpr bool enableValidationLayers = false;
+#endif
+
+static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+    VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+    VkDebugUtilsMessageTypeFlagsEXT messageType,
+    const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+    void* pUserData) {
+
+    std::string severity = "INVALID SEVERITY";
+
+    // TODO: implement message severity filtering
+    if (messageSeverity & VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT) {
+        severity = "VERBOSE";
+    } else if (messageSeverity & VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT) {
+        severity = "INFO";
+    } else if (messageSeverity & VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT) {
+        severity = "WARNING";
+    } else if (messageSeverity & VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT) {
+        severity = "ERROR";
+    }
+
+    std::string type = "INVALID TYPE";
+
+    // TODO: implement message severity filtering
+    if (messageType & VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT) {
+        type = "GENERAL";
+    } else if (messageType & VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT) {
+        type = "VALIDATION";
+    } else if (messageType & VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT) {
+        type = "PERFORMANCE";
+    }
+
+    fmt::print("[VK] [{}] [{}] {}\n", severity, type, pCallbackData->pMessage);
+
+    return VK_FALSE;
+}
+
+bool checkValidationLayerSupport() {
+    uint32_t layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+    std::vector<VkLayerProperties> availableLayers(layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+    bool allLayersFound = true;
+    for (auto requestedLayer : validationLayers) {
+        bool layerPresent = false;
+
+        for (auto availableLayer : availableLayers) {
+            if (strcmp(requestedLayer, availableLayer.layerName) == 0) {
+                layerPresent = true;
+                break;
+            }
+        }
+
+        if (layerPresent) {
+            fmt::print("Layer {} is availabe\n", requestedLayer);
+        } else {
+            fmt::print("Layer {} is not available\n", requestedLayer);
+            allLayersFound = false;
+        }
+    }
+
+    return allLayersFound;
+}
+
+VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger) {
+    auto func = (PFN_vkCreateDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+    if (func != nullptr) {
+        return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
+    } else {
+        return VK_ERROR_EXTENSION_NOT_PRESENT;
+    }
+}
+
+void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator) {
+    auto func = (PFN_vkDestroyDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+    if (func != nullptr) {
+        func(instance, debugMessenger, pAllocator);
+    }
+}
+
+void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo) {
+    createInfo = {};
+    createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+    createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+    createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+    createInfo.pfnUserCallback = debugCallback;
+    createInfo.pUserData = nullptr; // Optional
+}
+
+void setupDebugMessenger() {
+    if (!enableValidationLayers) {
+        return;
+    }
+
+    VkDebugUtilsMessengerCreateInfoEXT createInfo{};
+    populateDebugMessengerCreateInfo(createInfo);
+
+    if (CreateDebugUtilsMessengerEXT(instance, &createInfo, nullptr, &debugMessenger) != VK_SUCCESS) {
+        throw std::runtime_error("failed to setup debug message hook");
+    }
+}
+
+void initWindow()
+{
+    // Initialize GLFW
+    if (!glfwInit()) {
+        fmt::print("Failed to init GLFW\n");
+        throw std::runtime_error("GLFW Init Error");
+    }
+
+    fmt::print("GLFW initialized\n");
+
+    if (glfwVulkanSupported())
+    {
+        // Vulkan is available, at least for compute
+        fmt::print("GLFW detected vulkan support\n");
+    } else {
+        fmt::print("GLFW did not detect vulkan support\n");
+        throw std::runtime_error("GLFW No Vulkan Support");
+    }
+
+    // Tell GLFW we don't want it to assume a client API (since GLFW defaults to OpenGL support)
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+    // Tell GLFW not to allow window resizing. Might revisit this later, but this is fine for now
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    window = glfwCreateWindow(window_width, window_height, "VK Engine", nullptr, nullptr);
+
+}
+
+std::vector<const char*> getRequiredInstanceExtensions() {
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensions;
+
+    // Query GLFW to figure out what extensions it needs from Vulkan
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+
+    if (enableValidationLayers) {
+        extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+    }
+
+    for (auto extension : extensions) {
+        fmt::print("Required Extension: {}\n", extension);
+    }
+
+    return extensions;
+}
+
+void createInstance()
+{
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Hello Triangle";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::vector<VkExtensionProperties> extensions(extensionCount);
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+    fmt::print("Available Vulkan Extensions:\n");
+    for (const auto& extension : extensions) {
+        fmt::print("\t{}\n", extension.extensionName);
+    }
+
+    std::vector<const char*> requiredExtensions = getRequiredInstanceExtensions();
+    
+    // Set the requested extensions from GLFW
+    createInfo.enabledExtensionCount = requiredExtensions.size();
+    createInfo.ppEnabledExtensionNames = requiredExtensions.data();
+
+    VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo{};
+    if (enableValidationLayers && checkValidationLayerSupport()) {
+        createInfo.enabledLayerCount = validationLayers.size();
+        createInfo.ppEnabledLayerNames = validationLayers.data();
+
+        populateDebugMessengerCreateInfo(debugCreateInfo);
+        createInfo.pNext = (VkDebugUtilsMessengerCreateInfoEXT*) &debugCreateInfo;
+    } else {
+        createInfo.enabledLayerCount = 0;
+    }
+
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+        throw std::runtime_error("Failed to create Vulkan instance");
+    }
+}
+
+void initVulkan()
+{
+    createInstance();
+    setupDebugMessenger();
+}
+
+void mainLoop()
+{
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+}
+
+void cleanup()
+{
+    if (enableValidationLayers) {
+        DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
+    }
+
+    vkDestroyInstance(instance, nullptr);
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+}
 
 int main(int argc, char** argv)
 {
@@ -25,24 +266,12 @@ int main(int argc, char** argv)
 
     CLI11_PARSE(app, argc, argv);
 
-    GLFWwindow* window;
+    initWindow();
+    initVulkan();
 
-    // Initialize GLFW
-    if (!glfwInit()) {
-        fmt::print("Failed to init GLFW\n");
-        return -1;
-    }
+    mainLoop();
 
-    fmt::print("GLFW initialized\n");
-
-    if (glfwVulkanSupported())
-    {
-        // Vulkan is available, at least for compute
-        fmt::print("GLFW detected vulkan support\n");
-    } else {
-        fmt::print("GLFW did not detect vulkan support\n");
-    }
-
+    cleanup();
 
     return 0;
 }
